6827ebdf78022bb4f85fcc7731d9a20a8832cfdf
LĐHuy <leduchuy2205@gitlab.com>
2021-10-01 15:06:03 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const PORT = 8080;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Test nodemon");
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log("Listening on port " + PORT);
+});
