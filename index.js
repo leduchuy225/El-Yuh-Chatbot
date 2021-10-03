6344ab1c59a84b49a2896a15b86b087024c6efa6
LĐHuy <leduchuy2205@gitlab.com>
2021-10-03 09:36:28 +0700

    add web hook

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express");
 
-const PORT = 8080;
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.use(express.json());
+
+app.get("/", (_, res) => {
   res.send("Welcome to my app");
 });
 
+app.use("/", require("./src/web-hook"));
 app.use("/news", require("./src/news"));
 
-app.listen(process.env.PORT || PORT, () => {
+app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
